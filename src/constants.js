70114dabd975960f1847d28e15bec7bb66469127
Anthony Astley Lim <anthony.lim@redflaggroup.com>
2018-10-30 13:35:59 +0800

    added opposites constants

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,20 @@
+const DIRECTION_UP = 'up'
+const DIRECTION_DOWN = 'down'
+const DIRECTION_LEFT = 'left'
+const DIRECTION_RIGHT = 'right'
+
 module.exports = Object.freeze({
-    DIRECTION_UP: 'up',
-    DIRECTION_DOWN: 'down',
-    DIRECTION_LEFT: 'left',
-    DIRECTION_RIGHT: 'right',
+    DIRECTION_UP,
+    DIRECTION_DOWN,
+    DIRECTION_LEFT,
+    DIRECTION_RIGHT,
     CHAR_HIGAD: '#',
     CHAR_FOOD: "\u2764",
     CHAR_SPACE: ' ',
+    OPPOSITES: {
+        DIRECTION_UP: DIRECTION_DOWN,
+        DIRECTION_DOWN: DIRECTION_UP,
+        DIRECTION_LEFT: DIRECTION_RIGHT,
+        DIRECTION_RIGHT: DIRECTION_LEFT,
+    }
 })
